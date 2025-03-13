104cdf24238df4bd26f2b2a0f7cfde0f61f7a657
Mathis Sedkaoui <mathis.sedkaoui@grenoble-inp.org>
2025-03-13 14:57:31 +0100

    Initial Commit

---
+++ HackatonMARGOTests/HackatonMARGOTests.swift
@@ -0,0 +1,17 @@
+//
+//  HackatonMARGOTests.swift
+//  HackatonMARGOTests
+//
+//  Created by Mathis Sedkaoui on 13/03/2025.
+//
+
+import Testing
+@testable import HackatonMARGO
+
+struct HackatonMARGOTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
